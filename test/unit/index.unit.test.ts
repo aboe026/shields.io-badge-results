c4e6e7f4b0c2f520ce055d3fd25679e1d2e14fed
Adam Boe <adamboe@outlook.com>
2023-04-13 18:16:10 -0700

    Fix coverage

--- test/unit/index.unit.test.ts
+++ test/unit/index.unit.test.ts
@@ -3,7 +3,8 @@ import Args from '../../src/args'
 import BadgeResult from '../../src/badge-result'
 
 describe('Index Unit Tests', () => {
-  it('calls setBadgeResult', async () => {
+  it('exits with non-zero code if error thrown', async () => {
+    const error = 'permission denied'
     jest.spyOn(Args, 'get').mockResolvedValue({
       repo: 'test-call-repo',
       branch: 'test-call-branch',
@@ -11,13 +12,15 @@ describe('Index Unit Tests', () => {
       message: 'test-call-message',
       color: COLOR.Green,
     })
-    const setBadgeResultsSpy = jest.spyOn(BadgeResult, 'set').mockResolvedValue()
+    const setBadgeResultsSpy = jest.spyOn(BadgeResult, 'set').mockRejectedValue(error)
     const exitSpy = jest.spyOn(process, 'exit').mockImplementation()
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation()
     await jest.isolateModules(async () => {
       await import('../../src/index')
     })
     await new Promise((resolve) => setTimeout(resolve, 0)) // need explicit sleep here because isolateModules does not properly await :/
     expect(setBadgeResultsSpy).toHaveBeenCalled()
-    expect(exitSpy.mock.calls).toEqual([])
+    expect(exitSpy.mock.calls).toEqual([[1]])
+    expect(consoleErrorSpy.mock.calls).toEqual([[error]])
   })
 })
